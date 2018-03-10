3fb4f90019a3b6430fb10944de25e630456179df
dmytroyurchyk <dimayrchuk@gmail.com>
2018-03-10 12:23:49 -0500

    Dratute

--- src/main/GooDMorning.java
+++ src/main/GooDMorning.java
@@ -1,5 +1,10 @@
 package main;
 
 public class GooDMorning {
+	public static void main(String[] args) {
+		System.out.println("Good Morning!");
+		
+		System.out.println("This is Dimassss");
+	}
 
 }
